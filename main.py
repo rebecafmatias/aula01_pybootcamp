8fc338525398f822752a7390eb1d2fd71fefa84a
rebecafmatias <rebecafeitosamatias@gmail.com>
2024-04-22 14:15:08 -0300

    finalização desafio calculadora PLR_vf

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
-# """ Criar um programa que o usuário digite o seu nome e retorne o número de caracteres """
+""" Criar um programa que o usuário digite o seu nome e retorne o número de caracteres """
 
-# nome = input('Digite seu nome: ')
-# len_nome = len(nome)
+nome = input('Digite seu nome: ')
+len_nome = len(nome)
 
-# print(f'Seu nome tem {len_nome} caracteres, {nome}.')
+print(f'Seu nome tem {len_nome} caracteres, {nome}.')
 
-# """ Criar um programa onde o usuário digite dois valores e apareça a soma """
+""" Criar um programa onde o usuário digite dois valores e apareça a soma """
 
-# num1 = float(input('Primeiro valor: '))
-# num2 = float(input('Segundo valor: '))
+num1 = float(input('Primeiro valor: '))
+num2 = float(input('Segundo valor: '))
 
-# resultado = num1 + num2
+resultado = num1 + num2
 
-# print(f'Resultado: {resultado}')
+print(f'Resultado: {resultado}')
 
 """ Cálculo de Bônus com Entrada do Usuário """
 
