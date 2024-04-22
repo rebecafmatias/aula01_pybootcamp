40476d34f57fd9af37eb813cc62ecf964e3cf7d7
rebecafmatias <rebecafeitosamatias@gmail.com>
2024-04-22 10:44:19 -0300

    aula finalizada

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-# print('olá! tudo bem, Rebeca!')
+""" Criar um programa que o usuário digite o seu nome e retorne o número de caracteres """
 
+nome = input('Digite seu nome: ')
+len_nome = len(nome)
 
-nome =  input('Digite seu nome: ')
-saudacao = f'olá! tudo bem, {nome}'
+print(f'Seu nome tem {len_nome} caracteres, {nome}.')
 
-print(saudacao)
+""" Criar um programa onde o usuário digite dois valores e apareça a soma """
+
+num1 = float(input('Primeiro valor: '))
+num2 = float(input('Segundo valor: '))
+
+resultado = num1 + num2
+
+print(f'Resultado: {resultado}')
